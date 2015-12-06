de2454dd434f3f31845cfb5c70bddcaa20b601c2
David Cheng <davidlimcheng@gmail.com>
2015-12-06 01:52:50 -0800

    Added parseFile unit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var fs = require('fs');
 var inquirer = require('inquirer');
+var csv = require('ya-csv');
 var Promise = require('bluebird');
+
 Promise.promisifyAll(fs);
 Promise.promisifyAll(inquirer);
+Promise.promisifyAll(csv);
 
-var getFile = function() {
-  var allFiles = [];
+var getFilename = function() {
+  var allCSVFiles = [];
 
   //Get list of all csv files in directory
   return new Promise(function(resolve) {
@@ -14,23 +17,22 @@ var getFile = function() {
         for (var index in files) {
           var extension = files[index].split('.');
           if (extension[1] === 'csv') {
-            allFiles.push(files[index]);
+            allCSVFiles.push(files[index]);
           }
         }
 
-        return allFiles;
+        return allCSVFiles;
       })
-      .then(function(allFiles) {
+      .then(function(allCSVFiles) {
         //Present list to user and get chosen file
         inquirer.promptAsync([
             {
               type: 'list',
               name: 'userChoice',
               message: 'Choose a file:',
-              choices: allFiles,
+              choices: allCSVFiles,
             },
           ], function(answer) {
-          console.log('You have chosen: ' + answer.userChoice);
           resolve(answer.userChoice);
         });
       })
@@ -41,5 +43,32 @@ var getFile = function() {
 };
 
 var parseFile = function() {
-
+  var domains = [];
+  return new Promise(function(resolve) {
+    getFilename()
+      .then(function(filename) {
+        var reader = csv.createCsvFileReader('csvFiles/' + filename);
+        return reader;
+      })
+      .then(function(reader) {
+        reader.addListenerAsync('data', function(data) {
+          domains.push(data[0]);
+          resolve(domains);
+        });
+      })
+      .catch(function(err) {
+        throw err;
+      });
+  });
 };
+
+var runDig = function() {
+  return new Promise(function(resolve) {
+    parseFile()
+      .then(function(domains) {
+        console.log(domains);
+      });
+  });
+};
+
+runDig();
